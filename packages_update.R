7f6a4160d182aeb33188343db47badc65f12e5e5
Joseph Larmarange <joseph@larmarange.net>
2022-09-13 12:14:24 +0200

    package chapter

---
+++ packages_update.R
@@ -0,0 +1,9 @@
+# Script pour installer / mettre à jour les packages utilisés
+
+if (!requireNamespace("remotes")) install.packages("remotes")
+remotes::install_cran("renv")
+remotes::install_cran("purrr")
+
+renv::dependencies() |> 
+  purrr::pluck("Package") |> 
+  remotes::install_cran()
