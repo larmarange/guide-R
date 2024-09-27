2db6f7bc8efbadda6786d398d84be3ebf9a42dc8
Joseph Larmarange <joseph@larmarange.net>
2024-09-27 02:46:27 +0200

    sélection pas à pas modèle de Cox et amélioration de step_with_na

--- analyses/step_with_na.R
+++ analyses/step_with_na.R
@@ -1,21 +1,37 @@
 step_with_na <- function(model,
                          full_data = NULL, ...) {
-  # assign the model in parent environment
-  assign("...full.model", model, pos = 1) 
+  # data with no NAs
+  if (is.null(full_data)) {
+    ...data_no_na <- model.frame(model)
+  } else {
+    ...data_no_na <- get_all_vars(model, data = full_data)
+    ...data_no_na <- ...data_no_na[complete.cases(...data_no_na), ]
+  }
+  
+  # assign ...data_no_na in parent.frame()
+  assign("...data_no_na", ...data_no_na, envir = parent.frame())
+  
   # refit the model without NAs
   model_no_na <- update(
-    ...full.model,
-    formula = terms(...full.model),
-    data = model.frame(...full.model))
+    model,
+    formula = terms(model),
+    data = ...data_no_na
+  )
+  
   # apply step()
   model_simplified <- step(model_no_na, ...)
+  
   # recompute simplified model using full data
   if (is.null(full_data)) {
-    update(model,
-           formula = terms(model_simplified))
+    update(
+      model,
+      formula = terms(model_simplified)
+    )
   } else {
-    update(model,
-           formula = terms(model_simplified),
-           data = full_data)
+    update(
+      model,
+      formula = terms(model_simplified),
+      data = full_data
+    )
   }
 }
